e6f31e5d8e5a8cef65011acd45c8fa6b406216a3
Emory Griffith <emorygriffith@Emorys-MacBook-Air.local>
2014-10-07 17:29:58 -0400

    got the hamburger element on

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,10 +1,13 @@
 
 
 var cont= $('#drink'),
-    title, price, currency_code, image, shop_name, glass;
+    title, price, currency_code, image, shop_name, glass, box;
+
 
 
 whiskey.results.forEach ( function(item) {
+
+  box = "<img class= 'box' src='" + 'https://raw.githubusercontent.com/tiy-atlanta-js/Assignments/master/Assignment%2008/assets/hamburger.png' + "'/>"
 
   title = "<p class='t'>" + item.title + "</p>";
 
@@ -16,8 +19,20 @@ whiskey.results.forEach ( function(item) {
 
   image = "<img src='" + item.Images[0].url_170x135 + "'/>";
 
-  glass = "<li>" + image + title + shop_name + currency_code + price + "</li>";
+  glass = "<li>" + box + image + title + shop_name + currency_code + price + "</li>";
+
+
 
   cont.append(glass);
 
 });
+
+
+//
+// $('li').hover(
+//
+//   function() {
+//     $(this).toggleClass('showButtons');
+//   },
+//
+// );
