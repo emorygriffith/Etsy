a0956ac7e2a78b46d1b1b77fcfd2413dd61fc13e
Emory Griffith <emorygriffith@Emorys-MacBook-Air.local>
2014-10-07 15:22:19 -0400

    finished styling boxes and hover effect

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -6,17 +6,17 @@ var cont= $('#drink'),
 
 whiskey.results.forEach ( function(item) {
 
-  title = "<p>" + item.title + "</p>";
+  title = "<p class='t'>" + item.title + "</p>";
 
-  price = "<p>" + item.price + "</p>";
+  price = "<p class='p'>" + item.price + "</p>";
 
-  currency_code = "<p>" + item.currency_code + "</p>";
+  currency_code = "<p class='cc'>" + item.currency_code + "</p>";
 
-  shop_name = "<p>" + item.Shop.shop_name + "</p>";
+  shop_name = "<p class='sn'>" + item.Shop.shop_name + "</p>";
 
   image = "<img src='" + item.Images[0].url_170x135 + "'/>";
 
-  glass = "<li>" + image + title + shop_name + price + currency_code +  "</li>";
+  glass = "<li>" + image + title + shop_name + currency_code + price + "</li>";
 
   cont.append(glass);
 
